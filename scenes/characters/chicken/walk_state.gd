71d25b46922640c2cb5fa187437813787d3f0c5d
pankrok <mr.pankrok@gmail.com>
2025-01-11 18:46:43 +0100

    npc chicken

--- scenes/characters/chicken/walk_state.gd
+++ scenes/characters/chicken/walk_state.gd
@@ -1,6 +1,6 @@
 extends NodeState
 
-@export var character: CharacterBody2D
+@export var character: NonPlayableCharacter
 @export var animated_sprite_2d: AnimatedSprite2D
 @export var navigation_agent_2d: NavigationAgent2D;
 @export var minimum_speed: float = 5.0
@@ -29,6 +29,7 @@ func _on_process(_delta : float) -> void:
 
 func _on_physics_process(_delta : float) -> void:
 	if navigation_agent_2d.is_navigation_finished():
+		character.walk_cycles = 1;
 		set_movement_target()
 		return
 
@@ -37,24 +38,27 @@ func _on_physics_process(_delta : float) -> void:
 	animated_sprite_2d.flip_h = target_direction.x < 0
 
 	var velocity: Vector2 = target_direction * speed
-	if (navigation_agent_2d.avoidance_enabled):
+	if navigation_agent_2d.avoidance_enabled:
+		animated_sprite_2d.flip_h = velocity.x < 0
 		navigation_agent_2d.velocity = velocity
 	else:
 		character.velocity = velocity
 		character.move_and_slide()
 
 func on_safe_velosity_computed(save_velocity: Vector2) -> void:
+	animated_sprite_2d.flip_h = save_velocity.x < 0
 	character.velocity = save_velocity
 	character.move_and_slide()
 
 func _on_next_transitions() -> void:
-	if navigation_agent_2d.is_navigation_finished():
+	if character.current_walk_cycle == character.walk_cycles:
 		character.velocity = Vector2.ZERO
 		transition.emit("Idle")
 	pass
 
 
 func _on_enter() -> void:
+	character.current_walk_cycle = 0
 	animated_sprite_2d.play("walk")
 	pass
 
--- scenes/characters/non_playable_characer.gd
+++ scenes/characters/non_playable_characer.gd
@@ -1,6 +1,11 @@
 class_name NonPlayableCharacter
 extends CharacterBody2D
 
+@export var min_walk_cycle: int = 2
+@export var max_walk_cycle: int = 6
+
+var walk_cycles: int
+var current_walk_cycle: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
